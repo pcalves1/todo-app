2c8b7e89ed31dcf241c89e2b7fde1af93242e278
Paulo Alves <pauloc.alves@live.com>
2024-05-08 15:50:06 -0300

    compose ok

--- src/persistence/dbConfig.js
+++ src/persistence/dbConfig.js
@@ -1,12 +1,12 @@
-const mysql = require("mysql2/promise")
 const fs = require('fs')
 
 const {
   MYSQL_HOST: HOST,
   MYSQL_HOST_FILE: HOST_FILE,
   MYSQL_USER: USER,
+  MYSQL_PASSWORD: USER_PASSWORD,
   MYSQL_USER_FILE: USER_FILE,
-  MYSQL_ROOT_PASSWORD: PASSWORD,
+  MYSQL_ROOT_PASSWORD: ROOT_PASSWORD,
   MYSQL_ROOT_PASSWORD_FILE: PASSWORD_FILE,
   MYSQL_DATABASE: DB,
   MYSQL_DATABASE_FILE: DB_FILE,
@@ -14,15 +14,16 @@ const {
 
 const host = HOST_FILE ? fs.readFileSync(HOST_FILE) : HOST
 const user = USER_FILE ? fs.readFileSync(USER_FILE) : USER
-const password = PASSWORD_FILE ? fs.readFileSync(PASSWORD_FILE) : PASSWORD
+const passwordRoot = PASSWORD_FILE ? fs.readFileSync(PASSWORD_FILE) : ROOT_PASSWORD
+const passwordUser = PASSWORD_FILE ? fs.readFileSync(PASSWORD_FILE) : USER_PASSWORD
 const database = DB_FILE ? fs.readFileSync(DB_FILE) : DB
 
-let pool = mysql.createPool({
+let dbOpts = {
   host,
   user,
-  password,
+  password: passwordRoot,
   database,
   port: 3306,
-});
+};
 
-module.exports = pool
+module.exports = dbOpts
